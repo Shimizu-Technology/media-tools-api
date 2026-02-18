e7637377fbc4b960959a0a0a267e75cef2a8c80c
Jerry <jerry.shimizutechnology@gmail.com>
2026-02-19 00:21:21 +1000

    Fix Greptile round 10: safe down migration with USING LEFT()

--- migrations/022_widen_youtube_id_column.down.sql
+++ migrations/022_widen_youtube_id_column.down.sql
@@ -1,3 +1,3 @@
 -- Revert youtube_id back to VARCHAR(20).
--- Note: existing rows with values longer than 20 chars will cause this to fail.
-ALTER TABLE transcripts ALTER COLUMN youtube_id TYPE VARCHAR(20);
+-- Uses LEFT() to truncate any values longer than 20 chars (e.g., Vimeo/generic IDs).
+ALTER TABLE transcripts ALTER COLUMN youtube_id TYPE VARCHAR(20) USING LEFT(youtube_id, 20);
