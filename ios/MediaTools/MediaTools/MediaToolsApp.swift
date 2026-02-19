5fabe1e43f66771a6927ac3289026556d20d78c3
Jerry <jerry.shimizutechnology@gmail.com>
2026-02-20 08:53:42 +1000

    fix: remove PhoneConnectivityService reference from MediaToolsApp

--- ios/MediaTools/MediaTools/MediaToolsApp.swift
+++ ios/MediaTools/MediaTools/MediaToolsApp.swift
@@ -5,9 +5,6 @@ import ClerkKit
 struct MediaToolsApp: App {
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
 
-    // Initialize Watch connectivity on app launch
-    @StateObject private var phoneConnectivity = PhoneConnectivityService.shared
-
     init() {
         Clerk.configure(publishableKey: Configuration.clerkPublishableKey)
         configureAppearance()
